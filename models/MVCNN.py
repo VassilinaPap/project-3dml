a3683893e00682002994d4c96409f7122c63ebb5
unknown <chgeorgakilas@gmail.com>
2023-01-12 20:07:39 +0200

    changes

--- models/MVCNN.py
+++ models/MVCNN.py
@@ -1,108 +1,134 @@
-import numpy as np
-import os
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
 import torchvision.models as models
-
-
-# mean = Variable(torch.FloatTensor([0.485, 0.456, 0.406]), requires_grad=False).cuda()
-# std = Variable(torch.FloatTensor([0.229, 0.224, 0.225]), requires_grad=False).cuda()
-
-# def flip(x, dim):
-#     xsize = x.size()
-#     dim = x.dim() + dim if dim < 0 else dim
-#     x = x.view(-1, *xsize[dim:])
-#     x = x.view(x.size(0), x.size(1), -1)[:, getattr(torch.arange(x.size(1)-1, 
-#                       -1, -1), ('cpu','cuda')[x.is_cuda])().long(), :]
-#     return x.view(xsize)
-
-
-# class SVCNN(Model):
-
-#     def __init__(self, name, nclasses=13, pretraining=True, cnn_name='vgg11'):
-#         super(SVCNN, self).__init__(name)
-
-#         self.classnames=["airplane",     "bench",     "cabinet",     "car",     "chair",     "display",     "lamp",     "loudspeaker",     "rifle",
-#         "sofa",    "table",     "telephone",     "watercraft"]
-
-#         self.nclasses = nclasses
-#         self.pretraining = pretraining
-#         self.cnn_name = cnn_name
-#         self.mean = Variable(torch.FloatTensor([0.485, 0.456, 0.406]), requires_grad=False).cuda()
-#         self.std = Variable(torch.FloatTensor([0.229, 0.224, 0.225]), requires_grad=False).cuda()
-
-        
-#         if self.cnn_name == 'resnet18':
-#             self.net = models.resnet18(pretrained=self.pretraining)
-#             self.net.fc = nn.Linear(512,40)
-#         elif self.cnn_name == 'mobilenet_v3_large':
-#             self.net = models.mobilenet_v3_large(weights='IMAGENET1K_V2')
-#             self.net.fc = nn.Linear(1280,40)
-#         elif self.cnn_name == 'alexnet':
-#             self.net_1 = models.alexnet(pretrained=self.pretraining).features
-#             self.net_2 = models.alexnet(pretrained=self.pretraining).classifier
-#         elif self.cnn_name == 'vgg11':
-#             self.net_1 = models.vgg11(pretrained=self.pretraining).features
-#             self.net_2 = models.vgg11(pretrained=self.pretraining).classifier
-#         elif self.cnn_name == 'vgg16':
-#             self.net_1 = models.vgg16(pretrained=self.pretraining).features
-#             self.net_2 = models.vgg16(pretrained=self.pretraining).classifier
-            
-#             self.net_2._modules['6'] = nn.Linear(4096,40)
-
-#     def forward(self, x):
-#         if self.use_resnet:
-#             return self.net(x)
-#         else:
-#             y = self.net_1(x)
-#             return self.net_2(y.view(y.shape[0],-1))
-
 
 class MVCNN(nn.Module):
 
-    def __init__(self, nclasses=40, cnn_name='mobilenet_v3', num_views=3):
+    def __init__(self, n_classes=13, encoder='mobilenet_v3', num_views=3):
         super(MVCNN, self).__init__()
 
-        # self.classnames=['airplane','bathtub','bed','bench','bookshelf','bottle','bowl','car','chair',
-        #                  'cone','cup','curtain','desk','door','dresser','flower_pot','glass_box',
-        #                  'guitar','keyboard','lamp','laptop','mantel','monitor','night_stand',
-        #                  'person','piano','plant','radio','range_hood','sink','sofa','stairs',
-        #                  'stool','table','tent','toilet','tv_stand','vase','wardrobe','xbox']
-
-        self.nclasses = nclasses
+        self.n_classes = n_classes
         self.num_views = num_views
-        # self.mean = Variable(torch.FloatTensor([0.485, 0.456, 0.406]), requires_grad=False).cuda()
-        # self.std = Variable(torch.FloatTensor([0.229, 0.224, 0.225]), requires_grad=False).cuda()
+        self.encoder = encoder
+        self.decoder = None
+        self.fuse_cl_rec = None
+        self.decoder_volume = None
 
-
-        if cnn_name=='mobilenet_v3':
+        if self.encoder == 'mobilenet_v3':
             self.net = models.mobilenet_v3_large(pretrained=True)
-            self.net_1 = nn.Sequential(*list(self.net.children())[:-1])
-            self.net_2 = nn.Linear(960,13)
-        elif cnn_name=='resnet18':
+
+            self.net_1 = nn.Sequential(*list(self.net.children())[:-1])     #[B, 960, 137, 137]
+
+            self.net_2 = nn.Sequential(                                                 
+                                nn.Linear(960, 480), nn.BatchNorm1d(480), nn.ReLU(True),
+                                nn.Linear(480, self.n_classes)
+                                )
+
+        elif self.encoder == 'resnet18':
+
             self.net = models.resnet18(pretrained=True)
-            self.net_1 = nn.Sequential(*list(self.net.children())[:-1])
-            self.net_2 = nn.Linear(512,13)
+
+            self.net_1 = nn.Sequential(*list(self.net.children())[:-4])
+
+            self.net_2 = nn.Sequential(
+                                nn.Linear(512, self.n_classes)
+                                )
+
+
+        self.fuse_cl_rec = nn.Sequential(
+            nn.Conv1d(in_channels=960+self.n_classes, out_channels=960, kernel_size=1, padding=0),
+            nn.BatchNorm1d(960),
+            nn.ReLU()
+        )
+
+        self.decoder = nn.Sequential(
+            # Layer 1: out [B, 120, 4, 4, 4]
+            nn.ConvTranspose3d(in_channels=120, out_channels=64, kernel_size=4, stride=2, bias=False, padding=1),
+            nn.BatchNorm3d(64),
+            nn.ReLU(),
+            # Layer 2: out [B, 32, 8, 8, 8]
+            nn.ConvTranspose3d(in_channels=64, out_channels=32, kernel_size=4, stride=2, bias=False, padding=1),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+            # Layer 3: out [B, 16, 16, 16, 16]
+            nn.ConvTranspose3d(in_channels=32, out_channels=16, kernel_size=4, stride=2, bias=False, padding=1),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+            # Layer 4: out [B, 8, 32, 32, 32]
+            nn.ConvTranspose3d(in_channels=16, out_channels=8, kernel_size=4, stride=2, bias=False, padding=1),
+            nn.BatchNorm3d(8),
+            nn.ReLU(),
+        )
+
+        self.decoder_volume = nn.Sequential(
+            nn.Conv3d(in_channels=8, out_channels=1, kernel_size=1, bias=False),
+            nn.Sigmoid() # to deal with problem as a binary classification task
+        )
+
+
 
     def forward(self, x):
-        # [batch=2, views=4, 3, 137, 137] #
-        x = x.transpose(0, 1)
-        # [views=4, batch=2, 3, 137, 137] #
+        # x [batch, views, 3, 137, 137] #
+        batch_size = x.shape[0]
+        x = x.transpose(0, 1) # [views, batch, 3, 137, 137]
+        #print(x.shape)          
+        
         feature_list = []
-        for view in x:
-            view_features = self.net_1(view)
-            view_features = view_features.view(view_features.shape[0], -1)
+        for view in x: # [batch, 3, 137, 137]
+            view_features = self.net_1(view) # [batch, 960, 1, 1]
+            #print(view_features.shape)
+            view_features = view_features.view(view_features.shape[0], -1) # [batch, 960]
+            #print(view_features.shape)
             feature_list.append(view_features)
 
-        # 4 -> [2, 290] #
-        # feature_list  #
         max_features = feature_list[0]
         for view_features in feature_list[1:]:
+            max_features = torch.max(max_features, view_features)# [batch, 960]
+        #print(max_features.shape)
+        
+        """
+        v = x.shape[0]
+        b = x.shape[1]
+        c = x.shape[2]
+        w = x.shape[3]
+        h = x.shape[4]
+
+        x = x.reshape(v * b, c, w, h)
+
+        # Pass to encoder - example mobilenet dimensions #
+        view_features = self.net_1(x) # [views * batch, 960]
+
+        view_features = view_features.reshape(v, b, 960) # [views, batch, 960]
+
+        # [batch, 960] #
+        max_features = view_features[0]
+        for view_features in view_features[1:]:
             max_features = torch.max(max_features, view_features)
+        """
+                #classification branch
+        cl_ret = self.net_2(max_features) # [batch, classes]
+        #print(cl_ret)
+        #reconstruction branch
+        #pred_labels = torch.argmax(cls_ret, dim=1)[:, None, None, None]
+        #print(torch.cat((max_features, cl_ret), dim=1).shape)
 
-        # [2, 290] #
-        ret = self.net_2(max_features)
+        #print(max_features.shape)
+        max_features = max_features[:, :, None ]
+        #print(max_features.shape)
 
-        return ret
+        #print(cl_ret.shape)
+        cl_ret3d = cl_ret[:, :, None ]
+
+        #print(cl_ret.shape)
+        #print(cl_ret3d.shape)
+        features = self.fuse_cl_rec(torch.cat((max_features, cl_ret3d), dim=1))
+
+        features = features.view(batch_size, -1, 2, 2, 2) # [B, C, 2, 2, 2]
+
+        decoded_features = self.decoder(features)
+        #print(decoded_features.shape)
+
+        generated_volume = self.decoder_volume(decoded_features) # [B, 1, 32, 32, 32]
+        rec_ret = generated_volume.squeeze()
+
+        return cl_ret, rec_ret
--- training/train_mvcnn.py
+++ training/train_mvcnn.py
@@ -7,39 +7,63 @@ from MVCNN import MVCNN
 sys.path.append('../src')
 from dataset import ShapeNetDataset
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+import matplotlib.pyplot as plt
+import cv2
+import random
+import torch.nn.functional as F
 
+plt.switch_backend('agg')
 
+def ioU(predictions_rec, voxel):
+    # prob to vox grid
+    predictions_rec[predictions_rec >= 0.5] = 1
+    predictions_rec[predictions_rec < 0.5] = 0
+
+    intersection = (predictions_rec + voxel)
+    #intersection = torch.count_nonzero(intersection == 2).item()
+    intersection = torch.numel(intersection[intersection==2])
+    union = predictions_rec.sum().item() + voxel.sum().item()
+    return (intersection/union) * 100
 
 def train(model, train_dataloader, val_dataloader, device, config):
     # TODO: Declare loss and move to device; we need both smoothl1 and pure l1 losses here
-    loss_criterion = nn.CrossEntropyLoss().to(device)
-    
+    loss_criterion_cl = nn.CrossEntropyLoss().to(device)
+    loss_criterion_rec = nn.BCELoss().to(device)
+
     # TODO: Declare optimizer with learning rate given in config
     optimizer = torch.optim.Adam(model.parameters(),config['learning_rate'])
 
-    # Here, we follow the original implementation to also use a learning rate scheduler -- it simply reduces the learning rate to half every 20 epochs
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+    #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.1, patience=10)
+
+    logger = SummaryWriter()
 
     # TODO: Set model to train
     model.train()
     best_loss_val = np.inf
     best_accuracy = 0.
-    # Keep track of running average of train loss for printing
-    train_loss_running = 0.
+    best_iou = 0.
 
     for epoch in range(config['max_epochs']):
+        train_loss_running = 0.
+        train_iou = 0.
         for batch_idx, batch in enumerate(train_dataloader):
             # TODO: Move batch to device, set optimizer gradients to zero, perform forward pass
             ShapeNetDataset.move_batch_to_device(batch, device)
             optimizer.zero_grad()
-            predictions = model(batch['images']) 
-            _, predicted_labels = torch.max(predictions, dim=1)
-            target = batch['label']
-           
+            predictions_cl,predictions_rec = model(batch['images'])
 
+            _, predicted_labels = torch.max(predictions_cl, dim=1)
+            target = batch['label']
+            voxel = batch['voxel']
             # TODO: Compute loss, Compute gradients, Update network parameters
-            loss = loss_criterion(predictions, target)
+            loss_cl = loss_criterion_cl(predictions_cl, target)
+            loss_rec = loss_criterion_rec(predictions_rec,voxel)
+            loss = 0.5*loss_cl + 0.5*loss_rec
+            iou = ioU(predictions_rec.detach().clone(),voxel)
+            train_iou += iou
             loss.backward()
+
             # TODO: update network params
             optimizer.step()
             # Logging
@@ -49,44 +73,90 @@ def train(model, train_dataloader, val_dataloader, device, config):
             if iteration % config['print_every_n'] == (config['print_every_n'] - 1):
                 print(f'[{epoch:03d}/{batch_idx:05d}] train_loss: {train_loss_running / config["print_every_n"]:.6f}')
                 train_loss_running = 0.
+                train_iou = 0.
 
-            # Validation evaluation and logging
-            if iteration % config['validate_every_n'] == (config['validate_every_n'] - 1):
-                # TODO: Set model to eval
-                model.eval()
-                # Evaluation on entire validation set
-                loss_val = 0.
+        logger.add_scalar('loss/train_classification', train_loss_running / config["print_every_n"], epoch)
 
-                total = 0.0
-                correct = 0.0
-                for batch_val in val_dataloader:
-                    ShapeNetDataset.move_batch_to_device(batch_val, device)
+        # batch['images'] -> [batch, views, 3, 137, 137] # 
+        # predicted_labels -> [batch]                    #
+        logger.add_figure('predictions vs. actuals', plot_classes_preds(batch['images'], predicted_labels, predictions_cl, target, batch['class']), epoch)
 
-                    with torch.no_grad():
-                        predictions = model(batch_val['images'])
-                        _, predicted_labels = torch.max(predictions, dim=1)
-                        val_loss = loss_criterion(predictions, batch_val['label'])
+        # Validation evaluation and logging
+        # TODO: Set model to eval
+        model.eval()
+        # Evaluation on entire validation set
+        loss_val = 0.
+        val_iou = 0.
+        total = 0.0
+        correct = 0.0
+        for batch_val in val_dataloader:
+            ShapeNetDataset.move_batch_to_device(batch_val, device)
 
-                    total += predicted_labels.shape[0]
-                    correct += (predicted_labels == batch_val["label"]).sum().item()
+            with torch.no_grad():
+                predictions_cl,predictions_rec = model(batch_val['images'])
+                _, predicted_labels = torch.max(predictions_cl, dim=1)
+                val_loss_cl = loss_criterion_cl(predictions_cl, batch_val['label'])
+                val_loss_rec = loss_criterion_rec(predictions_rec, batch_val['voxel'])
+                val_loss = 0.5 * val_loss_cl + 0.5 * val_loss_rec
+                iou = ioU(predictions_rec.detach().clone(),batch_val['voxel'])
+                val_iou += iou
 
-                    loss_val += val_loss.item()
 
-                loss_val /= len(val_dataloader)
-                if loss_val < best_loss_val:
-                    torch.save(model.state_dict(), f'./project/runs/{config["experiment_name"]}/model_best_loss.ckpt')
-                    best_loss_val = loss_val
+            total += predicted_labels.shape[0]
+            correct += (predicted_labels == batch_val["label"]).sum().item()
 
-                print(f'[{epoch:03d}/{batch_idx:05d}] val_loss: {loss_val:.6f} | best_loss_val: {best_loss_val:.6f}')
+            loss_val += val_loss.item()
 
-                accuracy = 100 * correct / total
+        loss_val /= len(val_dataloader)
+        logger.add_scalar('Validation/loss', loss_val, epoch)
+        if loss_val < best_loss_val:
+            torch.save(model.state_dict(), f'./project/runs/{config["experiment_name"]}/model_best_loss.ckpt')
+            best_loss_val = loss_val
 
-                if accuracy > best_accuracy:
-                    torch.save(model.state_dict(), f'./project/runs/{config["experiment_name"]}/model_best_acc.ckpt')
-                    best_accuracy = accuracy
+        print(f'[{epoch:03d}] val_loss: {loss_val:.6f} | best_loss_val: {best_loss_val:.6f}')
 
+        accuracy = 100 * correct / total
+        print('\nAccuracy:' + '{:5}'.format(correct) + '/' +
+      '{:5}'.format(total) + ' (' +
+      '{:4.2f}'.format(100.0 * correct / total) + '%)\n') 
+        logger.add_scalar('Validation/Acc', accuracy, epoch)
+        if accuracy > best_accuracy:
+            torch.save(model.state_dict(), f'./project/runs/{config["experiment_name"]}/model_best_acc.ckpt')
+            best_accuracy = accuracy
+
+        val_iou /= len(val_dataloader)
+        if val_iou > best_iou:
+            torch.save(model.state_dict(), f'./project/runs/{config["experiment_name"]}/model_best_iou.ckpt')
+            best_iou = val_iou
+
+        logger.add_scalar('Validation/IoU', val_iou, epoch)
+        print(f'[{epoch:03d}] IoU: {val_iou:.6f} | best_iou: {best_iou:.6f}')
         model.train()
-        scheduler.step()
+        #scheduler.step(loss_val)
+
+# plot the images in the batch, along with predicted and true labels
+def plot_classes_preds(images, predicted_labels, predictions, labels, classes):
+    # batch['images'] -> [batch, views, 3, 137, 137] # 
+    fig = plt.figure(figsize=(10, 5))
+
+    probs_max, _ = torch.max(F.softmax(predictions, dim=1), dim=1)
+
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx+1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx][0])
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            ShapeNetDataset.index_to_class(predicted_labels[idx].item()),
+            probs_max[idx] * 100,
+            classes[idx]),
+                    color=("green" if labels[idx]==predicted_labels[idx].item() else "red"))
+
+    return fig 
+
+def matplotlib_imshow(img):
+    img = ShapeNetDataset.denormalize_image(img)
+    img = img.permute(1, 2, 0).numpy().astype("uint8")
+
+    plt.imshow(img)
 
 
 def main(config):
@@ -114,7 +184,7 @@ def main(config):
         print('Using CPU')
 
     # Create Dataloaders
-    train_dataset = ShapeNetDataset(split='train' if not config['is_overfit'] else 'overfit')
+    train_dataset = ShapeNetDataset(split='train' if not config['is_overfit'] else 'overfit', num_views=config['num_views'], augmentation_json_flag=config['augmentation_json_flag'], augmentations_flag=config['augmentations_flag'])
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,   # Datasets return data one sample at a time; Dataloaders use them and aggregate samples into batches
         batch_size=config['batch_size'],   # The size of batches is defined here
@@ -124,7 +194,7 @@ def main(config):
         # worker_init_fn=train_dataset.worker_init_fn  TODO: Uncomment this line if you are using shapenet_zip on Google Colab
     )
 
-    val_dataset = ShapeNetDataset(split='val' if not config['is_overfit'] else 'overfit')
+    val_dataset = ShapeNetDataset(split='val' if not config['is_overfit'] else 'overfit', num_views=config['num_views'], augmentation_json_flag=config['augmentation_json_flag'], augmentations_flag=config['augmentations_flag'])
     val_dataloader = torch.utils.data.DataLoader(
         val_dataset,     # Datasets return data one sample at a time; Dataloaders use them and aggregate samples into batches
         batch_size=config['batch_size'],   # The size of batches is defined here
@@ -135,7 +205,7 @@ def main(config):
     )
 
     # Instantiate model
-    model = MVCNN()
+    model = MVCNN(num_views=config['num_views'])
 
     # Load model if resuming from checkpoint
     if config['resume_ckpt'] is not None:
@@ -153,16 +223,24 @@ def main(config):
 if __name__ == "__main__":
 # Init parser to receive arguments from the terminal
 
+    # Seeds #
+    torch.manual_seed(15)
+    random.seed(15)
+    np.random.seed(15)
+
     config = {
     'experiment_name': 'mvcnn_overfitting',
     'device': 'cuda:0',  # change this to cpu if you do not have a GPU
     'is_overfit': True,
-    'batch_size': 2,
+    'batch_size': 8,
     'resume_ckpt': None,
-    'learning_rate': 0.00005,
+    'learning_rate': 0.00001,
     'max_epochs': 250,
     'print_every_n': 10,
     'validate_every_n': 25,
+    'num_views': 24,
+    'augmentation_json_flag': False,
+    'augmentations_flag': False
     }
 
     main(config)
